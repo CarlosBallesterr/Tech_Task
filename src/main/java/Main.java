9a24eef9ac2ddc570187f69ae2558c454e4a0575
CarlosBallesterr <carlos.ballester@jalasoft.com>
2025-09-17 18:59:40 -0400

    update Main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import Factory.FormatType;
 import Factory.SerializerFactory;
+import Logic.LoadMovieTypes;
 import Logic.RentalCalculator;
 import Models.*;
 import Serializer.DataSerializer;
@@ -9,37 +10,13 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // JSON para lista de MovieTypeEntry
-        Type movieListType = new TypeToken<List<MovieTypeEntry>>() {}.getType();
-        DataSerializer<List<MovieTypeEntry>> jsonSerializer =
-                SerializerFactory.getJsonSerializer(movieListType);
+    public static void main(String[] args) {
+        Customer customer = new Customer("Carlos");
+        customer.addRental(new Rental(new Movie("Zack Snyder's Justice League", 1), 5));
+        customer.addRental(new Rental(new Movie("Terminator", 0), 1));
+        customer.addRental(new Rental(new Movie("Soul", 2), 3));
 
-        List<MovieTypeEntry> movieTypes = jsonSerializer.read("src/main/resources/MovieTypeEntry.JSON");
-
-        for (MovieTypeEntry movieTypeEntry : movieTypes) {
-            //System.out.println(movieTypeEntry);
-        }
-
-        Type resultListType = new TypeToken<List<ResultEntry>>() {}.getType();
-        DataSerializer<List<ResultEntry>> jsonSerializer2 =
-                SerializerFactory.getJsonSerializer(resultListType);
-
-        List<ResultEntry> resultTypes = jsonSerializer2.read("src/main/resources/ResultEntry.JSON");
-
-        for (ResultEntry resultTypeEntry : resultTypes) {
-            System.out.println(resultTypeEntry);
-        }
-
-        //jsonSerializer.write(movieTypes, "src/main/resources/movieTypes_out.json");
-
-        /*
-        // XML para ResultEntry
-        DataSerializer<ResultEntry> xmlSerializer =
-                SerializerFactory.getXmlSerializer(ResultEntry.class);
-
-        ResultEntry result = xmlSerializer.read("result.xml");
-        xmlSerializer.write(result, "result_out.xml");
-         */
+        RentalCalculator rentalCalculator = new RentalCalculator(customer);
+        System.out.println(rentalCalculator.calculate());
     }
 }
